12d9a098fd4ce43d4303ab2a75374249042a8e58
cpcodex <cpcodex23@gmail.com>
2024-12-29 02:27:07 -0500

    update

--- app.py
+++ app.py
@@ -1,32 +1,31 @@
 import file_manager
 
 
-def data_seperator():
+def seperator():
     # Seperator
     return "=" * 40
-
-
-# define file path for ease of use
-file_name = file_manager.file_path
 
 
 def introduction():
     print(
         " Welcome to the Terminal Bank Account.\n",
-        data_seperator(),
+        seperator(),
         "\n You can input 'R' to Read, 'L' to Lookup, or 'S' to Save from the data file.\n"
         " You may also input 'Q' to quit the program.\n",
     )
+    return
 
+
+# define file path for ease of use
+file_name = file_manager.file_path
 
 # Print program introduction
 introduction()
 
 
 def main():
-    # Run
+    # while loop to begin program
     while True:
-        # while loop to begin program
 
         # input used to access program
         fix = input("Would you like to read, lookup, or save the data?\n").capitalize()
@@ -34,15 +33,15 @@ def main():
         if fix == "S":
             # Save user data
             file_manager.save_user_data(file_name)
-            data_seperator()
+            seperator()
         elif fix == "R":
             # Read user data
             file_manager.read_user_data(file_name)
-            data_seperator()
+            seperator()
         elif fix == "L":
             # search user data
             file_manager.search_user_data(file_name)
-            data_seperator()
+            seperator()
         elif fix == "Q":
             # Quit program
             print("Exiting input debugger...")
